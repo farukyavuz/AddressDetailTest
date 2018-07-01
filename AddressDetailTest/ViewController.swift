c42b4e321853a42f3501a84f2eb6c9d9636a8ae7
Faruk Yavuz <faruk@email.com>
2018-07-01 11:11:46 +0300

    Initial Commit

---
+++ AddressDetailTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AddressDetailTest
+//
+//  Created by Faruk Yavuz on 1.07.2018.
+//  Copyright © 2018 Faruk Yavuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
